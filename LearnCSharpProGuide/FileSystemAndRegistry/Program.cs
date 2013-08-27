77899187d9ce80a9db266c41baf560aedb314df2
mmix <mmix1009@gmail.com>
2013-08-27 22:31:59 +0800

    add FileSystemAndRegistty

---
+++ LearnCSharpProGuide/FileSystemAndRegistry/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+namespace FileSystemAndRegistry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           TestDriveInfo.Start();   
+
+            Console.ReadKey();
+        }
+    }
+}
